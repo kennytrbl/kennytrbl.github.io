f47628594f739bca2e52370fc6ccdb5e09eab72e
kennytrbl <kennyzhang418@gmail.com>
2024-07-24 23:37:51 -0700

    local push

--- src/App.js
+++ src/App.js
@@ -1,30 +1,10 @@
-import { Routes, Route, Link } from "react-router-dom";
-import About from "./About";
 import "./style/App.css";
+import About from "./About";
 
 function App() {
   return (
-    <div className="app">
-      {/* eslint-disable */}
-      <div className="nav-menu-items">
-        <ul>
-          <li>
-            <Link to="/">About</Link>
-          </li>
-          <li>
-            <a
-              href="https://drive.google.com/file/d/1HiWjAGNcKjxoN5Ke5-bG9mL0qq7Eqkwo/view?usp=sharing"
-              target="_blank"
-            >
-              Resume
-            </a>
-          </li>
-        </ul>
-      </div>
-
-      <Routes basename={process.env.PUBLIC_URL}>
-        <Route path="/" element={<About />} />
-      </Routes>
+    <div className="App">
+      <About />
     </div>
   );
 }
